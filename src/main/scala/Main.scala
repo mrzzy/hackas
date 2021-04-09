8f016d396e6962e099d0b61d3d3aab7b7c3dfdf5
Zhu Zhanyan <program.nom@gmail.com>
2021-04-09 18:31:03 +0800

    feat: Add usage string for Hackas to Main

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,3 +1,16 @@
+/*
+ * Hackas - HACK Assembler
+ * Main / Entrypoint
+*/
+package co.mrzzy.nand2tetris.p1.project6;
+
 object Main extends App {
-  println("Hello, World!")
+  val usage = """Usage: hackas [--output OUTPUT] INPUT
+  Assemble the HACK Assembly read from INPUT into HACK Machine Language.
+  Write the assembled machine language to OUTPUT.
+
+  Options:
+    --output  The path of the file to write the assembled machine language to.
+              Defaults to 'a.out'
+  """
 }
